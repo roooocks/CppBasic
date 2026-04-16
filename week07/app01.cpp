6e77e638c53b6758a11e359ce027aa8eda4b6d48
roooocks <dspdaw@gmail.com>
2026-04-16 12:16:06 +0900

    week07: heap memory management v0.4

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -55,7 +55,33 @@ void second()
 
 int main()
 {
-	second();
+	int rows = 0, columns = 0;
+	int** arr = nullptr;
+
+	cin >> rows;
+	cin >> columns;
+
+	arr = new int*[rows]; // 힙메모리에 포인터 배열 동적할당
+	for (int i = 0; i < rows; i++) {
+		arr[i] = new int[columns]; // 힙메모리에 정수 타입을 담는 배열 동적 할당
+		for (int j = 0; j < columns; j++) {
+			arr[i][j] = i * j;
+		}
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = rows - 1; i >= 0; i--) {
+		delete[] arr[i];
+		arr[i] = nullptr;
+	}
+	delete[] arr;
+	arr = nullptr;
 
 	return 0;
 }
