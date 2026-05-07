578c41c4634a008f631300462a44cb85c56a95ea
roooocks <dspdaw@gmail.com>
2026-05-07 10:54:12 +0900

    week10: literal is array and compact initializer v0.2

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -7,20 +7,33 @@ using namespace std;
 
 int main()
 {
-	// string이 지금은 빨라도, 코드가 많거나 데이터가 많아질수록 느려진다.
-	// 내부에서 동적 할당을 계속 해줘서 그런거다. (오버헤드 발생)
-	char a[] = "ACE";
-	char b[] = "ACE";
-	string c = "ACE";
-	string d = "ACE";
+	//cout << "01. string start" << endl;
+	//// string이 지금은 빨라도, 코드가 많거나 데이터가 많아질수록 느려진다.
+	//// 내부에서 동적 할당을 계속 해줘서 그런거다. (오버헤드 발생)
+	//char a[] = "ACE";
+	//char b[] = "ACE";
+	//string c = "ACE";
+	//string d = "ACE";
 
-	cout << strcmp(a, b) << endl; // 0(똑같음) / -1, 1(두 문자열이 다를 때)
-	cout << (c == d) << endl; // 1(true), 0(false)
+	//cout << strcmp(a, b) << endl; // 0(똑같음) / -1, 1(두 문자열이 다를 때)
+	//cout << (c == d) << endl; // 1(true), 0(false)
 
-	// a = "XYZ" // error
-	c = "XYZ!"; // success
+	//// a = "XYZ" // error
+	//c = "XYZ!"; // success
+	//cout << c << endl;
 
-	cout << c << endl;
+
+	//cout << "02. const, literal" << endl;
+	//// char* p = "ACE"; // error
+	//const char* p = "ACE"; // string literal(constant, read-only)
+	//cout << p << endl;
+
+
+	cout << "03. array, literal, compact initializer" << endl;
+	char a[] = "Hello"; // compact initializer, const char a[] = {'H', 'e', 'l', ..., '\0'};
+	const char b[] = "Hello"; // compact initializer
+	const char* c = "Hello"; // string literal
+
 
 	return 0;
 }
