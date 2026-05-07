764ba88f09b366443e4e507ce3b9c49c1d639a6c
roooocks <dspdaw@gmail.com>
2026-05-07 10:36:23 +0900

    week10: string v0.1

---
+++ week10/app01.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	// string이 지금은 빨라도, 코드가 많거나 데이터가 많아질수록 느려진다.
+	// 내부에서 동적 할당을 계속 해줘서 그런거다. (오버헤드 발생)
+	char a[] = "ACE";
+	char b[] = "ACE";
+	string c = "ACE";
+	string d = "ACE";
+
+	cout << strcmp(a, b) << endl; // 0(똑같음) / -1, 1(두 문자열이 다를 때)
+	cout << (c == d) << endl; // 1(true), 0(false)
+
+	// a = "XYZ" // error
+	c = "XYZ!"; // success
+
+	cout << c << endl;
+
+	return 0;
+}
