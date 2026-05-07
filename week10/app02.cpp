68c26252e0eed8196218d32f81c8cb18fd49d6c9
roooocks <dspdaw@gmail.com>
2026-05-07 12:22:23 +0900

    week10: string size and capacity v0.5

--- week10/app02.cpp
+++ week10/app02.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -29,13 +30,38 @@ int myStrcmp(const char* a, const char* b)
 
 int main()
 {
-	cout << "05. custom comparing(strcmp, strncmp)" << endl;
-	const char* s1 = "Hi Daelim";
-	const char* s2 = "Hi Daeli";
+	//cout << "05. custom comparing(strcmp, strncmp)" << endl;
+	//const char* s1 = "Hi Daelim";
+	//const char* s2 = "Hi Daeli";
 
-	cout << strcmp(s1, s2) << endl;
-	//cout << strncmp(s1, s2, 2) << endl;
-	cout << myStrcmp(s1, s2) << endl;
+	//cout << strcmp(s1, s2) << endl;
+	////cout << strncmp(s1, s2, 2) << endl;
+	//cout << myStrcmp(s1, s2) << endl;
+
+
+	cout << "06. string is size, capacity" << endl;
+	// string s1; // string s1("ABC") 직접 초기화로도 사용 가능
+	// string s1(5, 'x'); // x를 5개 사용
+	string s1("ABC", 2); // 값 2개만 사용
+
+	// s1 = "ABC"; // 복사 초기화(연산자 오버로딩으로 생성자를 호출하도록 바꿔버린 방식)
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.capacity() << endl;
+	s1 = "defghijk";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.capacity() << endl;
+	s1 = "123456789012345";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.capacity() << endl;
+
+	// 여기서부터는 capacity값이 바뀐다.
+	// 계속 15였다가 31이 된다. OS마다 그리고 컴파일러마다 달라진다.
+	// 16 32 64... 처럼 해당 값에 걸리면 동적 재할당(다음 값으로 넘어감)한다. 이유는 당연히 '\0' 때문이다.
+	// 다만 기본은 16으로 본다. 자주 쓰기 때문이며, 15 + 1로 보면 편하다.
+	s1 = "1234567890123456";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.capacity() << endl;
+
 
 	return 0;
 }
