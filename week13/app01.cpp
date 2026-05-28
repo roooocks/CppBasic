48c5003dc8ec70e9f07eb4d5551b8255e9435438
roooocks <dspdaw@gmail.com>
2026-05-28 11:32:42 +0900

    week13: main change v0.3

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 int main()
 {
-    Pickachu pickachu(500, 4000); // 2. 매개변수 생성자
+    Pickachu pickachu(500, 4000);
     cout << pickachu.getHp() << endl;
     cout << pickachu.getElectricAttack() << endl;
 
-    Pickachu pickachu2(pickachu); // 3. 복사 생성자
+    Pickachu pickachu2(pickachu);
     cout << pickachu2.getHp() << endl;
     cout << pickachu2.getElectricAttack() << endl;
 
